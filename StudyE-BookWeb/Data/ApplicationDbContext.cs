00024828ecabe4f225d039e64ceb69afd7865884
DoraNenoy <darja.leonova.it@gmail.com>
2022-09-29 15:58:37 +0300

    Connected to Sql

---
+++ StudyE-BookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyE_BookWeb.Models;
+
+namespace StudyE_BookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
